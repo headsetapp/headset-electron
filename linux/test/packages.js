65c2213e45ba48bb5a48892ba744a49cb2d61128
Felipe Castillo <fcastillo.ec@gmail.com>
2017-08-29 21:10:44 -0400

    Add initial test scripts

---
+++ linux/test/packages.js
@@ -0,0 +1,28 @@
+var Application = require('spectron').Application
+var assert = require('assert')
+const path = require('path');
+
+const appPath = path.join(__dirname, '..', 'build', 'headset-linux-x64', 'headset');
+
+describe('packages', function (test) {
+  this.timeout(10000)
+
+  beforeEach(function () {
+    this.app = new Application({
+      path: appPath
+    })
+    return this.app.start()
+  })
+
+  afterEach(function () {
+    if (this.app && this.app.isRunning()) {
+      return this.app.stop()
+    }
+  })
+
+  it('start application', function () {
+    return this.app.client.getWindowCount().then(function (count) {
+      assert.equal(count, 2)
+    })
+  })
+})
