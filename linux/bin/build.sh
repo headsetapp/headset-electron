802bef6f1ad306bad016e1caf6ae362b6d505eb1
Felipe Castillo <fcastillo.ec@gmail.com>
2017-09-08 05:28:28 -0400

    Improvements to build script It also fixes file permission to avoid lintian warnings and errors

--- linux/bin/build.sh
+++ linux/bin/build.sh
@@ -2,24 +2,36 @@
 
 set -e
 
+source="build/headset-linux-x64"
+destination="build/installers"
+ignoring=(bin test Procfile)
+
 electron-packager . headset \
-  --ignore bin \
+  --ignore $ignoring \
   --prune true \
   --out build/ \
-  --overwrite  \
+  --overwrite \
+  --asar true \
   --platform=linux \
   --arch=x64
 
-echo -e "\n\033[1mPackaging deb installer:\033[01;0m"
+echo -e "\n\033[1mFixing various file permissions: \033[01;0m"
+chmod 0644 $source/libnode.so
+chmod 0644 $source/resources/app.asar
+chmod 0755 $source/resources
+chmod 0755 $source/locales
+echo -e "Done"
+
+echo -e "\n\033[1mPackaging deb installer: \033[01;0m"
 electron-installer-debian \
-  --src build/headset-linux-x64/ \
-  --dest build/installers/ \
+  --src $source \
+  --dest $destination \
   --arch amd64 \
   --config bin/config_deb.json
 
-echo -e "\n\033[1mPackaging rpm installer:\033[01;0m"
+echo -e "\n\033[1mPackaging rpm installer: \033[01;0m"
 electron-installer-redhat \
-  --src build/headset-linux-x64/ \
-  --dest build/installers/ \
+  --src $source \
+  --dest $destination \
   --arch x86_64 \
   --config bin/config_rpm.json
