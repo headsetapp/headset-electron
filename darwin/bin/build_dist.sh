a79727efa433f572fbb8df0893abfb7c24689503
Felipe Castillo <fcastillo.ec@gmail.com>
2017-10-10 01:11:55 -0400

    Add OSX support

---
+++ darwin/bin/build_dist.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+echo -e '\n\033[1mPackaging dmg installer: \033[01;0m'
+mkdir -p build/installers
+zip -ryq "Headset@$NEW_VERSION.zip" build/Headset-darwin-x64/Headset.app
+create-dmg build/Headset-darwin-x64/Headset.app
+mv "Headset-$NEW_VERSION.dmg" build/installers/.
