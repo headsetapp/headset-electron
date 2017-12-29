3f26fb1c8e2ee89c3a2b3796fe943b62221fd265
Felipe Castillo <fcastillo.ec@gmail.com>
2017-12-29 17:20:21 -0500

    Fix bash lintian errors, and prevent errors on Travis from forks

--- darwin/bin/build_pack.sh
+++ darwin/bin/build_pack.sh
@@ -18,7 +18,7 @@ rm -R Icon.iconset
 
 ignoring="(bin|test|Procfile)"
 
-if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
+if [[ "$TRAVIS_OS_NAME" == "osx" && "$TRAVIS_REPO_SLUG" == "headsetapp/headset-electron" ]]; then
   KEY_CHAIN=mac-build.keychain
   security create-keychain -p travis $KEY_CHAIN
   # Make the keychain the default so identities are found
@@ -29,9 +29,9 @@ if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
   security set-keychain-settings -t 3600 -u $KEY_CHAIN
 
   # Add certificates to keychain and allow codesign to access them
-  security import $TRAVIS_BUILD_DIR/darwin/sig/apple.cer -k $KEY_CHAIN -A /usr/bin/codesign
-  security import $TRAVIS_BUILD_DIR/darwin/sig/osx.cer -k $KEY_CHAIN -A /usr/bin/codesign
-  security import $TRAVIS_BUILD_DIR/darwin/sig/osx.p12 -k $KEY_CHAIN -P $CERT_PASSWORD -A /usr/bin/codesign
+  security import "$TRAVIS_BUILD_DIR"/darwin/sig/apple.cer -k $KEY_CHAIN -A /usr/bin/codesign
+  security import "$TRAVIS_BUILD_DIR"/darwin/sig/osx.cer -k $KEY_CHAIN -A /usr/bin/codesign
+  security import "$TRAVIS_BUILD_DIR"/darwin/sig/osx.p12 -k $KEY_CHAIN -P "$CERT_PASSWORD" -A /usr/bin/codesign
 
   echo "Add keychain to keychain-list"
   security list-keychains -s $KEY_CHAIN
