75c121f8d817fa2ab1051baa05170aa8cac2a56a
Daniel Ravina <raabhim@gmail.com>
2017-12-29 21:03:58 -0800

    move zip to the installers directory

--- darwin/bin/build_dist.sh
+++ darwin/bin/build_dist.sh
@@ -2,6 +2,8 @@
 
 echo -e '\n\033[1mPackaging dmg installer: \033[01;0m'
 mkdir -p build/installers
-zip -ryq "Headset@$NEW_VERSION.zip" build/Headset-darwin-x64/Headset.app
+zip -ryq "Headset-$NEW_VERSION.zip" build/Headset-darwin-x64/Headset.app
 create-dmg build/Headset-darwin-x64/Headset.app
+
 mv "Headset-$NEW_VERSION.dmg" build/installers/.
+mv "Headset-$NEW_VERSION.zip" build/installers/.
