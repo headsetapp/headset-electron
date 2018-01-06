a3772385b7f369c0597a4f8aef2d909ae7b19488
Felipe Castillo <fcastillo.ec@gmail.com>
2018-01-05 22:11:24 -0500

    Show debugging logs when testing for OSX

--- darwin/test/app.test.js
+++ darwin/test/app.test.js
@@ -1,31 +1,52 @@
 const { Application } = require('spectron');
+const { exec } = require('child-process-promise');
 const assert = require('assert');
 const path = require('path');
 
 const appPath = path.join(__dirname, '..', 'build/Headset-darwin-x64/Headset.app/Contents/MacOS', 'Headset');
+let app = null;
 
 describe('application', function () {
   this.timeout(10000);
 
-  before(() => {
-    this.app = new Application({
+  before((done) => {
+    app = new Application({
       path: appPath,
+      env: {
+        DEBUG: 'headset*',
+      },
     });
-    return this.app.start();
+    app.start()
+      .then(() => setTimeout(() => {
+        assert.equal(app.isRunning(), true, 'App is not running');
+        done();
+      }, 2000))
+      .catch(err => done(err));
   });
 
-  after(() => {
-    if (this.app && this.app.isRunning()) {
-      return this.app.stop();
-    }
-    throw new Error('App is not running');
-  });
+  after(() => app.stop());
 
-  it('start application', () => {
-    if (this.app && this.app.isRunning()) {
-      this.app.client.getWindowCount().then((count) => {
-        assert.equal(count, 2);
+  // Displays all the DEBUG logs after each test is run
+  afterEach(() => app.client
+    .getMainProcessLogs().then((logs) => {
+      logs.forEach((log) => {
+        log = log.split(' ');
+        if (!Number.isNaN(Date.parse(log[0]))) {
+          console.log('\t\x1b[33m%s \x1b[34m%s\x1b[0m', log[0], log[1], log.splice(2).join(' '));
+        }
       });
-    }
-  });
+    }));
+
+  // Tests that both windows were loaded
+  it('start application', () => app.client
+    .waitUntilWindowLoaded()
+    .getWindowCount().then((count) => {
+      assert.equal(count, 2, 'Wrong number of windows');
+    }));
+
+  // Simulate pressing a media keys
+  it('send media keys', () => Promise.all([
+    exec('cliclick kp:play-pause'),
+    exec('cliclick kp:play-next'),
+    exec('cliclick kp:play-previous')]));
 });
