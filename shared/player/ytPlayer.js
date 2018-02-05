17b380b862c9fcabf9a51fdb56997d5b6e63681c
Daniel Ravina <raabhim@gmail.com>
2018-02-04 22:53:45 -0800

    redesign the player window

--- shared/player/ytPlayer.js
+++ shared/player/ytPlayer.js
@@ -112,8 +112,8 @@ window.onYouTubePlayerAPIReady = () => {
   let yt;
 
   ytPlayer = new YT.Player('ytplayer', {
-    width: '262',
-    height: '200',
+    width: '427',
+    height: '240',
     controls: 0,
     disablekb: 1,
     fs: 0,
