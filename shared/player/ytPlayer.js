ce6714d338ed85a74d0d0862dae497b6e8ea37a9
Daniel Ravina <raabhim@gmail.com>
2018-03-06 13:35:36 -0800

    update foreman and change player params for better usability

--- shared/player/ytPlayer.js
+++ shared/player/ytPlayer.js
@@ -114,9 +114,14 @@ window.onYouTubePlayerAPIReady = () => {
   ytPlayer = new YT.Player('ytplayer', {
     width: '427',
     height: '240',
-    controls: 0,
-    disablekb: 1,
-    fs: 0,
+    playerVars: {
+      'showinfo': 0,
+      'controls': 0,
+      'color': 'white',
+      'disablekb': 0,
+      'modestbranding': 0,
+      'widget_referrer': 'https://danielravina.github.io'
+    },
     events: {
       onReady() {
         yt = new YTPlayer();
