7eec554bbf356efca3027c35505f130bbae73b7d
Felipe Castillo <fcastillo.ec@gmail.com>
2018-03-11 19:55:48 -0400

    Switch electron-winstaller with electron-installer-windows

--- windows/bin/winstaller.js
+++ windows/bin/winstaller.js
@@ -1,20 +1,21 @@
-const electronInstaller = require('electron-winstaller');
+const installer = require('electron-installer-windows');
 
-console.log('\x1b[1m%s\x1b[0m', 'Packaging Windows installer:');
+const options = {
+  src: 'build/Headset-win32-ia32',
+  dest: 'build/installers/',
+  productDescription: 'Headset is a desktop app that turns YouTube into a world class music streaming service.' +
+      'Create collections, tune-in to a music subreddit or quickly play that song ' +
+      'you’ve had stuck in your head all day!',
+  iconUrl: 'https://raw.githubusercontent.com/headsetapp/headset-electron/master/windows/Headset.ico',
+};
+
 console.log('Creating package (this may take a while)');
 
-const output = 'build/installers';
+installer(options, (err) => {
+  if (err) {
+    console.error(err, err.stack);
+    process.exit(1);
+  }
 
-const resultPromise = electronInstaller.createWindowsInstaller({
-  appDirectory: 'build/Headset-win32-ia32',
-  outputDirectory: output,
-  authors: 'Alignment Digital',
-  exe: 'headset.exe',
-  noMsi: true,
-  iconUrl: 'https://raw.githubusercontent.com/headsetapp/headset-electron/master/windows/Headset.ico',
-  loadingGif: 'source.gif',
+  console.log(`Successfully created package at ${options.dest}`);
 });
-
-resultPromise
-  .then(() => console.log(`Successfully created installer at ${output}`))
-  .catch(error => console.error('\x1b[31m%s\x1b[0m', `Error creating installer: ${error.message}`));
