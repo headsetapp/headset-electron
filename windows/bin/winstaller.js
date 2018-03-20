52c5caf4992042d94ef60f99671d945926c53ef4
Felipe Castillo <fcastillo.ec@gmail.com>
2018-03-19 22:50:03 -0400

    Fix winstaller.js to use latest version of electron-installer-windows

--- windows/bin/winstaller.js
+++ windows/bin/winstaller.js
@@ -3,19 +3,20 @@ const installer = require('electron-installer-windows');
 const options = {
   src: 'build/Headset-win32-ia32',
   dest: 'build/installers/',
-  productDescription: 'Headset is a desktop app that turns YouTube into a world class music streaming service.' +
+  options: {
+    productDescription: 'Headset is a desktop app that turns YouTube into a world class music streaming service.' +
       'Create collections, tune-in to a music subreddit or quickly play that song ' +
       'you’ve had stuck in your head all day!',
-  iconUrl: 'https://raw.githubusercontent.com/headsetapp/headset-electron/master/windows/Headset.ico',
+    iconUrl: 'https://raw.githubusercontent.com/headsetapp/headset-electron/master/windows/Headset.ico',
+    licenseUrl: 'https://raw.githubusercontent.com/headsetapp/headset-electron/master/LICENSE',
+  },
 };
 
 console.log('Creating package (this may take a while)');
 
-installer(options, (err) => {
-  if (err) {
+installer(options)
+  .then(() => console.log(`Successfully created package at ${options.dest}`))
+  .catch((err) => {
     console.error(err, err.stack);
     process.exit(1);
-  }
-
-  console.log(`Successfully created package at ${options.dest}`);
-});
+  });
