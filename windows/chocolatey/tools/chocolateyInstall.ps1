f7f03cf2edce74b5b6cae7998b746a4c637e1249
Felipe Castillo <fcastillo.ec@gmail.com>
2018-04-05 19:36:42 -0700

    Add files to build chocolatey package

---
+++ windows/chocolatey/tools/chocolateyInstall.ps1
@@ -0,0 +1,14 @@
+﻿$version = '1.7.0'
+$url = "https://github.com/headsetapp/headset-electron/releases/download/v$version/HeadsetSetup.exe"
+
+$packageArgs = @{
+  packageName    = 'headset'
+  softwareName   = 'Headset'
+  fileType       = 'exe'
+  url            = $url
+  checksum       = 'NEEDS_TO_BE_REPLACED_BY_APPVEYOR!'
+  checksumType   = 'sha256'
+  validExitCodes = @(0)
+}
+
+Install-ChocolateyPackage @packageArgs
---
+++ windows/chocolatey/tools/chocolateyUninstall.ps1
@@ -0,0 +1,15 @@
+﻿$ErrorActionPreference = 'Stop';
+$name = 'headset'
+
+$packageArgs = @{
+  packageName = $name
+  softwareName = 'Headset'
+  fileType = 'exe'
+  validExitCodes = @(0)
+  file = "$env:LOCALAPPDATA\$name\Update.exe"
+  silentArgs = '--uninstall -s'
+}
+
+ Uninstall-ChocolateyPackage @packageArgs
+
+ Remove-Item -Path "$env:LOCALAPPDATA\$name" -Recurse -Force
