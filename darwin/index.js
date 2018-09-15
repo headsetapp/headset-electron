dd370cc3f5e0cc37f0583686494cc5361e2c6351
Felipe Castillo <fcastillo.ec@gmail.com>
2018-09-15 16:59:56 -0700

    Localize tray for macOS and change language when core sends a signal

--- darwin/index.js
+++ darwin/index.js
@@ -4,8 +4,10 @@ const defaultMenu = require('electron-default-menu');
 const windowStateKeeper = require('electron-window-state');
 const AutoUpdater = require('headset-autoupdater');
 const path = require('path');
-const { version } = require('./package');
+
 const headsetTray = require('./lib/headsetTray');
+const i18next = require('./lib/i18nextConfig');
+const { version } = require('./package');
 
 const logger = debug('headset');
 const logPlayer2Win = debug('headset:player2Win');
@@ -68,8 +70,15 @@ const start = () => {
     onBeforeQuit: () => { willQuitApp = true; },
   });
 
+  i18next.on('initialized', (options) => {
+    logger(`i18next has been initialized with ${JSON.stringify(options, null, 2)}`);
+  });
+
   tray = new Tray(path.join(__dirname, 'icons', 'Headset.png'));
-  headsetTray(tray, win, player);
+  i18next.on('loaded', () => {
+    logger('i18next resources loaded');
+    headsetTray(tray, win, player, i18next);
+  });
 
   win.webContents.on('did-finish-load', () => {
     logger('Main window finished loading');
@@ -172,4 +181,11 @@ ipcMain.on('player2Win', (e, args) => {
   try {
     win.webContents.send('player2Win', args);
   } catch (err) { /* window already closed */ }
+});
+
+ipcMain.on('change-locale', (e, lng) => {
+  logger(`Changing locale to: ${lng}`);
+
+  i18next.changeLanguage(lng);
+  headsetTray(tray, win, player, i18next);
 });
