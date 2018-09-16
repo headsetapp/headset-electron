4e007c230fea2eb46aed708965697fa24f6855d3
Felipe Castillo <fcastillo.ec@gmail.com>
2018-09-16 10:41:23 -0700

    Add namespace to i18next so scanner can handle the files without errors

--- darwin/lib/headsetTray.js
+++ darwin/lib/headsetTray.js
@@ -15,7 +15,7 @@ module.exports = (tray, win, player, i18n) => {
 
   const contextMenu = Menu.buildFromTemplate([
     {
-      label: i18n.t('Minimize'),
+      label: i18n.t('wrapper:Minimize'),
       click: () => {
         logger('Minimizing to tray');
         win.isVisible() ? win.hide() : win.show();
@@ -23,13 +23,13 @@ module.exports = (tray, win, player, i18n) => {
       },
     },
     { type: 'separator' },
-    { label: i18n.t('Play/Pause'), click: () => { executeTrayCommand(win, 'play-pause'); } },
-    { label: i18n.t('Next'), click: () => { executeTrayCommand(win, 'play-next'); } },
-    { label: i18n.t('Previous'), click: () => { executeTrayCommand(win, 'play-previous'); } },
+    { label: i18n.t('wrapper:Play/Pause'), click: () => { executeTrayCommand(win, 'play-pause'); } },
+    { label: i18n.t('wrapper:Next'), click: () => { executeTrayCommand(win, 'play-next'); } },
+    { label: i18n.t('wrapper:Previous'), click: () => { executeTrayCommand(win, 'play-previous'); } },
     { type: 'separator' },
-    { label: i18n.t('Like'), click: () => { executeTrayCommand(win, 'like'); } },
+    { label: i18n.t('wrapper:Like'), click: () => { executeTrayCommand(win, 'like'); } },
     { type: 'separator' },
-    { label: i18n.t('Exit'), role: 'quit' },
+    { label: i18n.t('wrapper:Exit'), role: 'quit' },
   ]);
 
   tray.setToolTip('Headset');
