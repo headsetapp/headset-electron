31e302597e6d1e958b8f50b57a090b96773cd9e5
Felipe Castillo <fcastillo.ec@gmail.com>
2018-11-01 18:10:25 -0700

    Upgrade mpris module This upgrade handles mediaKeys now, so the module can be safely removed

--- linux/index.js
+++ linux/index.js
@@ -3,7 +3,6 @@ const debug = require('debug');
 const electron = require('electron');
 const windowStateKeeper = require('electron-window-state');
 const mprisService = require('./lib/mprisService.js');
-const registerMediaKeys = require('./lib/registerMediaKeys.js');
 const { version } = require('./package');
 
 const logger = debug('headset');
@@ -87,7 +86,6 @@ const start = () => {
     try {
       logger('Initializing MPRIS and registering MediaKeys');
       mprisService(win, player);
-      registerMediaKeys(win);
     } catch (err) {
       console.error(err);
     }
