265d7f17bfe65a1ec393119c0fee56bba1fa61d0
Felipe Castillo <fcastillo.ec@gmail.com>
2019-06-01 20:11:05 -0700

    macOS: better support for create-dmg - Better checking for exit codes - Make sure Travis has the necessary packages to create a DMG icon

--- darwin/bin/build_installer.sh
+++ darwin/bin/build_installer.sh
@@ -6,7 +6,12 @@ echo -e '\n\033[1mPackaging dmg installer: \033[01;0m'
 mkdir -p build/installers
 zip -ryq "Headset-$NEW_VERSION.zip" build/Headset-darwin-x64/Headset.app
 
-set +e; create-dmg --overwrite build/Headset-darwin-x64/Headset.app; set -e
+set +e
+create-dmg --overwrite build/Headset-darwin-x64/Headset.app
+if ! [[ $? -eq 0 || $? -eq 2 ]]; then
+  exit 1
+fi
+set -e
 
 mv "Headset $NEW_VERSION.dmg" build/installers/"Headset-$NEW_VERSION.dmg"
 mv "Headset-$NEW_VERSION.zip" build/installers/.
