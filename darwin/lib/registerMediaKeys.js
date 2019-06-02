466d5c8c7e66dcd110a604330033309c8789d3e0
Felipe Castillo <fcastillo.ec@gmail.com>
2019-06-02 13:51:34 -0700

    Fix indentation

--- darwin/lib/registerMediaKeys.js
+++ darwin/lib/registerMediaKeys.js
@@ -7,21 +7,21 @@ module.exports = (win) => {
   globalShortcut.register('MediaPlayPause', () => {
     logger('Executing %o media key command', 'play-pause');
     win.webContents.executeJavaScript(`
-          window.electronConnector.emit('play-pause')
-        `);
+      window.electronConnector.emit('play-pause')
+    `);
   });
 
   globalShortcut.register('MediaNextTrack', () => {
     logger('Executing %o media key command', 'play-next');
     win.webContents.executeJavaScript(`
-          window.electronConnector.emit('play-next')
-        `);
+      window.electronConnector.emit('play-next')
+    `);
   });
 
   globalShortcut.register('MediaPreviousTrack', () => {
     logger('Executing %o media key command', 'play-previous');
     win.webContents.executeJavaScript(`
-          window.electronConnector.emit('play-previous')
-        `);
+      window.electronConnector.emit('play-previous')
+    `);
   });
 };
--- windows/lib/registerMediaKeys.js
+++ windows/lib/registerMediaKeys.js
@@ -5,21 +5,21 @@ module.exports = (win) => {
   globalShortcut.register('MediaPlayPause', () => {
     logger.media('Executing play-pause media key command');
     win.webContents.executeJavaScript(`
-        window.electronConnector.emit('play-pause')
-      `);
+      window.electronConnector.emit('play-pause')
+    `);
   });
 
   globalShortcut.register('MediaNextTrack', () => {
     logger.media('Executing play-next media key command');
     win.webContents.executeJavaScript(`
-        window.electronConnector.emit('play-next')
-      `);
+      window.electronConnector.emit('play-next')
+    `);
   });
 
   globalShortcut.register('MediaPreviousTrack', () => {
     logger.media('Executing play-previous media key command');
     win.webContents.executeJavaScript(`
-        window.electronConnector.emit('play-previous')
-      `);
+      window.electronConnector.emit('play-previous')
+    `);
   });
 };
