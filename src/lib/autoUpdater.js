87288ac16c764ad7e9d9e5d687e1f55f2fba0525
Felipe Castillo <fcastillo.ec@gmail.com>
2019-08-14 13:59:49 -0700

    Check for updates only when installed for Windows

--- src/lib/autoUpdater.js
+++ src/lib/autoUpdater.js
@@ -1,11 +1,13 @@
 
 const { app, autoUpdater } = require('electron');
+const fs = require('fs');
+const path = require('path');
 
 const SERVER = 'https://update.electronjs.org';
 
 class AutoUpdater {
   constructor(options = {}) {
-    const feed = `${SERVER}/headsetapp/headset-electron/${process.platform}-${process.arch}/${app.getVersion()}`;
+    const feed = `${SERVER}/headsetapp/headset-electron/${process.platform}/${app.getVersion()}`;
 
     autoUpdater.setFeedURL(feed);
 
@@ -14,11 +16,16 @@ class AutoUpdater {
       options.onUpdateDownloaded();
     });
 
-    autoUpdater.checkForUpdates();
-
-    setInterval(() => {
+    // Check if app is installed with Squirrel
+    if (process.platform === 'darwin'
+      || (process.platform === 'win32'
+          && fs.existsSync(path.resolve(path.dirname(process.execPath), '..', 'update.exe')))) {
       autoUpdater.checkForUpdates();
-    }, 10 * 60 * 1000);
+
+      setInterval(() => {
+        autoUpdater.checkForUpdates();
+      }, 10 * 60 * 1000);
+    }
   }
 
   /* eslint-disable class-methods-use-this */
