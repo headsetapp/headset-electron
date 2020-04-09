150d15691d12c2c6b6f84a90cf26868ffa187127
Felipe Castillo <fcastillo.ec@gmail.com>
2020-04-08 22:13:05 -0700

    Release 3.2.2

--- .chocolatey/tools/chocolateyInstall.ps1
+++ .chocolatey/tools/chocolateyInstall.ps1
@@ -1,4 +1,4 @@
-﻿$version = '3.2.1'
+﻿$version = '3.2.2'
 
 $url          = "https://github.com/headsetapp/headset-electron/releases/download/v$version/headset-$version-setup.exe"
 $autoStartKey = 'HKLM:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Run\'
