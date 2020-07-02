9af97ec61ced06633188091df95ef54358f04577
Felipe Castillo <1280530+fcastilloec@users.noreply.github.com>
2020-07-01 17:14:30 -0700

    Switch from Travis and Appveyor to Github Actions (#296)
    * Switch from Travis and Appveyor to Github Actions
    - Builds and test for all OSs
    - Fixes Windows showing certificate password on errors
    - Creates a Github release including the body
    - Creates a Linux repo with new encrypted cert and deploys
    - Publish to Chocolatey (not completely tested)
    TODO: Add sig/headset_priv.asc.gpg
    
    * fix: use new default branch (main)
    
    * update apple certificates
    
    * fix: use Github actions badge
    
    * add Linux repo key encrypted with GPG
    
    Co-authored-by: Daniel Ravina <raabhim@gmail.com>

--- bin/packager/darwin.sh
+++ bin/packager/darwin.sh
@@ -2,26 +2,32 @@
 
 set -e
 
-if [[ "$TRAVIS_OS_NAME" == "osx" && "$TRAVIS_REPO_SLUG" == "headsetapp/headset-electron" && -n "$TRAVIS_TAG" ]]; then
+if [[ "$GITHUB_REPOSITORY" == "headsetapp/headset-electron" && "$GITHUB_REF" == refs/tags/* ]]; then
   KEY_CHAIN=mac-build.keychain
-  security create-keychain -p travis $KEY_CHAIN
+  DIR="$GITHUB_WORKSPACE/sig"
+  PASSWORD=headset
+
+  echo "Creating default keychain"
+  security create-keychain -p $PASSWORD $KEY_CHAIN
   # Make the keychain the default so identities are found
   security default-keychain -s $KEY_CHAIN
   # Unlock the keychain
-  security unlock-keychain -p travis $KEY_CHAIN
+  security unlock-keychain -p $PASSWORD $KEY_CHAIN
   # Set keychain locking timeout to 3600 seconds
   security set-keychain-settings -t 3600 -u $KEY_CHAIN
 
   # Add certificates to keychain and allow codesign to access them
-  security import "$TRAVIS_BUILD_DIR"/sig/apple.cer -k $KEY_CHAIN -A /usr/bin/codesign
-  security import "$TRAVIS_BUILD_DIR"/sig/osx.cer -k $KEY_CHAIN -A /usr/bin/codesign
-  security import "$TRAVIS_BUILD_DIR"/sig/osx.p12 -k $KEY_CHAIN -P "$CERT_PASSWORD" -A /usr/bin/codesign
+  security import "$DIR"/apple.cer -k $KEY_CHAIN -A /usr/bin/codesign
+  security import "$DIR"/osx.cer -k $KEY_CHAIN -A /usr/bin/codesign
+  security import "$DIR"/osx.p12 -k $KEY_CHAIN -P "$CERT_PASSWORD" -A /usr/bin/codesign
 
   echo "Add keychain to keychain-list"
   security list-keychains -s $KEY_CHAIN
 
   echo "Settting key partition list"
-  security set-key-partition-list -S apple-tool:,apple: -s -k travis $KEY_CHAIN
+  security set-key-partition-list -S apple-tool:,apple: -s -k $PASSWORD $KEY_CHAIN
+else
+  printf "\x1b[33m%s\x1b[0m\n" "The package will not be signed"
 fi
 
 OS=darwin node bin/packager/packager.js
