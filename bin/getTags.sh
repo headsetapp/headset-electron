662022a3374475d986e97ed241f444aad5ddc35a
Felipe Castillo <1280530+fcastilloec@users.noreply.github.com>
2020-07-02 02:04:34 -0700

    Fix GitHub actions tags and Linux dependencies (#299)
    * CI: output tags without a leading 'v'
    
    * CI: use npm scripts instead of bash scripts

--- bin/getTags.sh
+++ bin/getTags.sh
@@ -4,11 +4,12 @@ set -e
 
 mapfile -t REFS < <(git rev-list --tags --max-count=2)
 
-NEWTAG=$(git describe --abbrev=0 --tags "${REFS[0]}")
-OLDTAG=$(git describe --abbrev=0 --tags "${REFS[1]}")
+CURRENT_TAG=$(git describe --abbrev=0 --tags "${REFS[0]}")
+printf '\x1b[32mCurrent tag: %s\x1b[0m\n' "${CURRENT_TAG#v}"
+printf "::set-output name=newtag::%s\n" "${CURRENT_TAG#v}"
 
-printf '\x1b[32mLatest tag: %s\x1b[0m\n' "$NEWTAG"
-printf '\x1b[32mPrevious tag: %s\x1b[0m\n' "$OLDTAG"
-
-printf "::set-output name=newtag::%s\n" "$NEWTAG"
-printf "::set-output name=oldtag::%s\n" "$OLDTAG"
+if [ ${#REFS[@]} -eq 2 ]; then
+  PREVIOUS_TAG=$(git describe --abbrev=0 --tags "${REFS[1]}")
+  printf '\x1b[32mPrevious tag: %s\x1b[0m\n' "${PREVIOUS_TAG#v}"
+  printf "::set-output name=oldtag::%s\n" "${PREVIOUS_TAG#v}"
+fi
