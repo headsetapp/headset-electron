9f8a6ee96e03b2dddbbd435b2802233f28bcb306
Felipe Castillo <1280530+fcastilloec@users.noreply.github.com>
2021-02-13 23:21:14 -0800

    build(linux): sign RedHat rpm and repository (#348)

--- bin/linux_repo.sh
+++ bin/linux_repo.sh
@@ -8,6 +8,7 @@ installers="build/installers"
 echo -e "\033[1mGPG import procedure:\033[01;0m"
 gpg2 --import headset_priv.asc
 echo 'digest-algo sha256' > "$HOME"/.gnupg/gpg.conf
+echo '%_gpg_name Headset' > "$HOME"/.rpmmacros
 echo -e 'Done'
 
 echo -e "\n\033[1mCreating DEBIAN repository:\033[01;0m"
@@ -16,5 +17,7 @@ echo -e 'Done'
 
 echo -e "\n\033[1mCreating READHAT repository files:\033[01;0m"
 cp "$installers/headset-$VERSION-1.x86_64.rpm" $root/redhat
+rpm --addsign "$root/redhat/headset-$VERSION-1.x86_64.rpm"
 createrepo "$root/redhat"
+gpg --detach-sign --armor "$root/redhat/repodata/repomd.xml"
 echo -e 'Done'
