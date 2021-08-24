470af326d4303fcb7989fba5d5276ad2bef581a0
Felipe Castillo <1280530+fcastilloec@users.noreply.github.com>
2021-08-24 13:03:08 -0700

    fix: don't show dialogs for updating errors (#404)
    Errors are still logged in the default file but users won't be disturbed by the dialog

--- src/lib/autoUpdater.js
+++ src/lib/autoUpdater.js
@@ -1,27 +1,34 @@
 const { app, autoUpdater } = require('electron');
+const logger = require('./headsetLogger');
 
 const SERVER = 'https://update.electronjs.org';
 
 class AutoUpdater {
   constructor(options = {}) {
-    const feed = `${SERVER}/headsetapp/headset-electron/${process.platform}/${app.getVersion()}`;
+    const url = `${SERVER}/headsetapp/headset-electron/${process.platform}/${app.getVersion()}`;
 
-    autoUpdater.setFeedURL(feed);
+    autoUpdater.setFeedURL({ url });
 
     autoUpdater.on('update-downloaded', () => {
+      logger.update('Update downloaded');
       options.onUpdateDownloaded();
     });
 
-    // Check if app is installed with Squirrel
+    autoUpdater.on('error', (error) => {
+      logger.update(error); // don't show error dialog to users
+    });
+
+    // First run on start
     autoUpdater.checkForUpdates();
 
     setInterval(() => {
       autoUpdater.checkForUpdates();
-    }, 10 * 60 * 1000);
+    }, 60 * 60 * 1000); // check for updates every hour
   }
 
   /* eslint-disable class-methods-use-this */
   resetAndInstall() {
+    logger.update('Installing update');
     autoUpdater.quitAndInstall();
   }
   /* eslint-enable */
