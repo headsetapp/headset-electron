473c216ecd1dd5d267d450ce8cab49f7cbe9c25d
Felipe Castillo <fcastillo.ec@gmail.com>
2022-05-06 16:25:05 -0700

    refactor: only add needed options for each OS when packaging

--- bin/packager/packager.js
+++ bin/packager/packager.js
@@ -4,10 +4,6 @@ const path = require('path');
 
 const { OS, CERT_PASSWORD } = process.env;
 
-let arch;
-let executableName;
-let icon;
-
 const ignore = [
   /^\/\.chocolatey$/,
   /^\/\.github$/,
@@ -26,39 +22,36 @@ const ignore = [
   /README\.md$/,
 ];
 
-if (OS === 'linux') {
-  executableName = 'headset';
-  icon = null;
-  arch = 'x64';
-}
-if (OS === 'win32') {
-  executableName = 'headset';
-  icon = path.join(__dirname, '..', '..', 'src', 'icons', 'headset.ico');
-  arch = 'ia32';
-}
-if (OS === 'darwin') {
-  executableName = 'Headset';
-  icon = path.join(__dirname, '..', '..', 'src', 'icons', 'headset.icns');
-  arch = 'x64';
-}
-
 const options = {
   dir: '.',
   out: 'build',
-  executableName,
-  icon,
-  arch,
+  executableName: 'headset',
+  arch: 'x64',
   platform: OS,
   asar: true,
   prune: true,
   overwrite: true,
-  osxSign: !!CERT_PASSWORD,
-  darwinDarkModeSupport: true,
-  appBundleId: 'co.headsetapp.app',
-  appCategoryType: 'public.app-category.music',
   ignore,
   afterCopy: [setLanguages(['en', 'en-US'])],
 };
+
+if (OS === 'win32') {
+  Object.assign(options, {
+    icon: path.join(__dirname, '..', '..', 'src', 'icons', 'headset.ico'),
+    arch: 'ia32',
+  });
+}
+
+if (OS === 'darwin') {
+  Object.assign(options, {
+    executableName: 'Headset',
+    icon: path.join(__dirname, '..', '..', 'src', 'icons', 'headset.icns'),
+    osxSign: !!CERT_PASSWORD,
+    darwinDarkModeSupport: true,
+    appBundleId: 'co.headsetapp.app',
+    appCategoryType: 'public.app-category.music',
+  });
+}
 
 async function main() {
   try {
