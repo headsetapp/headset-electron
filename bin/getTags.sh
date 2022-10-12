1299d60a76689d87a9915dc6e886d236b3858ea9
Felipe Castillo <fcastillo.ec@gmail.com>
2022-10-12 15:24:17 -0400

    ci: remove deprecated set-output

--- bin/getTags.sh
+++ bin/getTags.sh
@@ -6,10 +6,10 @@ mapfile -t REFS < <(git rev-list --tags --max-count=2)
 
 CURRENT_TAG=$(git describe --abbrev=0 --tags "${REFS[0]}")
 printf '\x1b[32mCurrent tag: %s\x1b[0m\n' "${CURRENT_TAG#v}"
-printf "::set-output name=newtag::%s\n" "${CURRENT_TAG#v}"
+echo "newtag=${CURRENT_TAG#v}" >> "${GITHUB_ENV:?}"
 
 if [[ ${#REFS[@]} -eq 2 ]]; then
   PREVIOUS_TAG=$(git describe --abbrev=0 --tags "${REFS[1]}")
   printf '\x1b[32mPrevious tag: %s\x1b[0m\n' "${PREVIOUS_TAG#v}"
-  printf "::set-output name=oldtag::%s\n" "${PREVIOUS_TAG#v}"
+  echo "oldtag=${PREVIOUS_TAG#v}" >> "${GITHUB_ENV:?}"
 fi
